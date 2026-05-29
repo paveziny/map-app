0c98a749c113a2ad167842a6ed5eb5fefbc65d46
Pave Ziny <paveziny@gmail.com>
2026-05-29 13:02:34 +0300

    Базовая настройка

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <v-app>
+    <div class="app-container">
+      <MapView />
+    </div>
+  </v-app>
 </template>
 
-<style scoped></style>
+<script setup>
+import MapView from '@/components/MapView.vue'
+</script>
+
+<style lang="scss" scoped>
+.app-container {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
+
+import 'ol/ol.css'
+
+import './assets/styles/main.scss'
+
+const vuetify = createVuetify({
+  theme: {
+    defaultTheme: 'light',
+  },
+})
+
 createApp(App).mount('#app')
